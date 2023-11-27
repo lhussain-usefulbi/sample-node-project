d6de0eac0fc73874ae5875f9ac3a07175a6d7d6a
lhussain-usefulbi <118711474+lhussain-usefulbi@users.noreply.github.com>
2023-11-27 11:38:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "7K/06c6]7;;5#Zg"WD]+o8[jMH.4Yi{p"
+  secret_key = "'8qj"0(o0R|@rDu?q4U9$£77{x7RkZX5'8qj"0(o0R|@rDu?q4U9$£77{x7RkZX5"
+}
